1b49c29821dffada4b4a543e70b6876c80c144cb
Rauulll <nyaokeraul003@gmail.com>
2023-08-21 21:26:55 +0300

    final toy_app

--- lib/toy_app_web/router.ex
+++ lib/toy_app_web/router.ex
@@ -17,7 +17,8 @@ defmodule ToyAppWeb.Router do
   scope "/", ToyAppWeb do
     pipe_through :browser
 
-    get "/", PageController, :hello
+    get "/", PageController, :home
+    resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
